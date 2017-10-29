2fbe6d3bbaa309f1f5fa5a6cc8ab8e90316da9d3
Andy Chapman <andy.chapman@propertysoftwaregroup.com>
2017-10-29 10:11:21 +0000

    Added package.d

---
+++ source/package.d
@@ -0,0 +1,9 @@
+module dcorelib;
+
+public
+{
+    import dcorelib.valueobjects.constrainedstring;
+    import dcorelib.valueobjects.id;
+    import dcorelib.valueobjects.varchar255required;
+    import dcorelib.valueobjects.varchar255;
+}
